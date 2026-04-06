b14c51e3067c66e702981ca177bab5d6e816d3be
Divyansh <kaushikdivyansh39@gmail.com>
2026-04-07 01:34:07 +0530

    fixed production backend url

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5002/api" : "/api",
+  baseURL: import.meta.env.MODE === "development"
+    ? "http://localhost:5002/api"
+    : "https://chatspace-sl3i.onrender.com/api",
   withCredentials: true,
 });
