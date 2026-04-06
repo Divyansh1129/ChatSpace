4af6783dbca02762c31edef6f13c45328999635f
Divyansh <kaushikdivyansh39@gmail.com>
2026-04-07 01:39:10 +0530

    fixed socket cors

--- Backend/src/lib/socket.js
+++ Backend/src/lib/socket.js
@@ -7,7 +7,11 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: [
+      "http://localhost:5173",
+      "https://chat-space-eta.vercel.app"
+    ],
+    credentials: true,
   },
 });
 
@@ -24,7 +28,7 @@ io.on("connection", (socket) => {
   const userId = socket.handshake.query.userId;
   if (userId) userSocketMap[userId] = socket.id;
 
-  // io.emit() is used to send events to all the connected clients
+  // send online users
   io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
   socket.on("disconnect", () => {
